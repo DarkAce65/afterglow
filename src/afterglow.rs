b2814c6b3c039f5128b45814823671de274bc056
DarkAce65 <tvdood99@gmail.com>
2023-04-02 14:54:38 -0400

    Connect segment mapping to led strip

--- src/afterglow.rs
+++ src/afterglow.rs
@@ -13,8 +13,8 @@ use nokhwa::Camera;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 use std::{
     cmp::Ordering,
-    io::{stdout, Write},
-    thread::sleep,
+    f64::consts::{PI, TAU},
+    thread,
     time::Duration,
 };
 
@@ -96,35 +96,88 @@ fn prompt_camera(camera_index: CameraIndex) -> Camera {
     camera
 }
 
+fn build_segment_map(num_leds: usize, width: u32, height: u32) -> Vec<Option<usize>> {
+    let mut segment_table: Vec<Option<usize>> =
+        Vec::with_capacity((width * height).try_into().unwrap());
+
+    let width = width as i32;
+    let height = height as i32;
+    let half_width = width / 2;
+    let half_height = height / 2;
+    let edge = half_width.min(half_height) / 2;
+
+    let theta_scalar = (num_leds as f64) / TAU;
+
+    for y in 0..height {
+        let dy = (y - half_height) as f64;
+        for x in 0..width {
+            let dx = (half_width - x) as f64;
+            segment_table.push(if dx.hypot(dy) >= edge.into() {
+                let theta = dy.atan2(dx) + PI;
+                let segment = ((theta * theta_scalar).floor() as usize).min(num_leds - 1);
+                Some(segment)
+            } else {
+                None
+            });
+        }
+    }
+
+    segment_table
+}
+
 fn main() {
     let camera_index = prompt_camera_device();
     let mut camera = prompt_camera(camera_index);
+
+    let resolution = camera.resolution();
+    let width = resolution.width();
+    let height = resolution.height();
+
+    let segment_map = build_segment_map(NUM_LEDS, width, height);
 
     camera.open_stream().expect("Unable to open stream");
 
     let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 16_000_000, Mode::Mode0)
         .expect("Unable to initialize SPI");
 
-    const NUM_LEDS: usize = 10;
-    let mut i = 0;
+    const NUM_LEDS: usize = 36;
     let mut led_strip: LEDStrip<NUM_LEDS> = LEDStrip::new();
-    loop {
-        led_strip.set_led(i, 0x000000);
-        i = (i + 1) % NUM_LEDS;
-        led_strip.set_led(i, 0xff0000);
 
-        print!("\r");
-        for j in 0..NUM_LEDS {
-            if j == i {
-                print!("*");
-            } else {
-                print!("-");
+    let frame_delay = Duration::from_millis((1000 / camera.frame_rate()).into());
+
+    loop {
+        let frame = camera.frame().expect("Unable to get frame from camera");
+        let decoded_image = frame.decode_image::<RgbFormat>().unwrap();
+
+        let mut led_values: [(u64, u64, u64); NUM_LEDS] = [(0, 0, 0); NUM_LEDS];
+        let mut counts: [u64; NUM_LEDS] = [0; NUM_LEDS];
+        for (index, pixel) in decoded_image.chunks_exact(3).enumerate() {
+            if let Some(segment) = segment_map[index] {
+                if counts[segment] == 0 {
+                    led_values[segment].0 = u64::from(pixel[0]).pow(2);
+                    led_values[segment].1 = u64::from(pixel[1]).pow(2);
+                    led_values[segment].2 = u64::from(pixel[2]).pow(2);
+                } else {
+                    led_values[segment].0 += u64::from(pixel[0]).pow(2);
+                    led_values[segment].1 += u64::from(pixel[1]).pow(2);
+                    led_values[segment].2 += u64::from(pixel[2]).pow(2);
+                }
+                counts[segment] += 1;
             }
         }
-        stdout().flush().unwrap();
+
+        for (index, led_value) in led_values.iter().enumerate() {
+            let (r, g, b) = led_value;
+            let count = counts[index];
+            let r = ((r / count) as f64).sqrt() as u32;
+            let g = ((g / count) as f64).sqrt() as u32;
+            let b = ((b / count) as f64).sqrt() as u32;
+            let color = r << 16 | g << 8 | b;
+            led_strip.set_led(index, color);
+        }
 
         spi.write(led_strip.get_spi_data())
             .expect("Failed to write SPI data");
-        sleep(Duration::from_millis(500));
+        thread::sleep(frame_delay);
     }
 }
