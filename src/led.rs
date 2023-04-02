67cc8817845fe370aec336269b980a97a34db627
DarkAce65 <tvdood99@gmail.com>
2023-04-01 22:48:48 -0400

    Restructure APA102DataFrame for simplicity

--- src/led.rs
+++ src/led.rs
@@ -2,24 +2,27 @@ use lazycell::LazyCell;
 
 #[derive(PartialEq)]
 #[cfg_attr(test, derive(Debug))]
-pub enum APA102DataFrame {
-    Start,
-    End,
-    Led(u8, u8, u8),
-}
+pub struct APA102DataFrame(u8, u8, u8);
 
 impl APA102DataFrame {
+    #[inline]
+    fn start_frame_spi_data() -> [u8; 4] {
+        [0x00; 4]
+    }
+
+    #[inline]
+    fn end_frame_spi_data() -> [u8; 4] {
+        [0xff; 4]
+    }
+
     fn led_frame(data: u32) -> Self {
         let [_, r, g, b] = data.to_be_bytes();
-        APA102DataFrame::Led(r, g, b)
+        APA102DataFrame(r, g, b)
     }
 
     fn get_spi_data(&self) -> [u8; 4] {
-        match self {
-            APA102DataFrame::Start => [0x00; 4],
-            APA102DataFrame::End => [0xff; 4],
-            APA102DataFrame::Led(r, g, b) => [0xff, *b, *g, *r],
-        }
+        let APA102DataFrame(r, g, b) = self;
+        [0xff, *b, *g, *r]
     }
 }
 
@@ -46,20 +49,26 @@ impl<const N: usize> LEDStrip<N> {
         if !self.spi_data.filled() {
             let num_end_frames = (N + 1) / 2;
             let mut spi_data = Vec::with_capacity(N + num_end_frames + 1);
-            spi_data.extend(APA102DataFrame::Start.get_spi_data());
+            spi_data.extend(APA102DataFrame::start_frame_spi_data());
 
             for frame in self.data.iter() {
                 spi_data.extend(frame.get_spi_data());
             }
 
             for _ in 0..num_end_frames {
-                spi_data.extend(APA102DataFrame::End.get_spi_data());
+                spi_data.extend(APA102DataFrame::end_frame_spi_data());
             }
 
             self.spi_data.fill(spi_data).ok();
         }
 
         self.spi_data.borrow().unwrap()
+    }
+
+    pub fn get_led(&self, index: usize) -> (u8, u8, u8) {
+        assert!(index < N, "index out of bounds");
+        let APA102DataFrame(r, g, b) = self.data[index];
+        (r, g, b)
     }
 
     pub fn set_led(&mut self, index: usize, color: u32) {
@@ -79,25 +88,25 @@ mod tests {
     #[test]
     fn it_builds_grayscale_frames() {
         let black = APA102DataFrame::led_frame(0x000000);
-        assert_eq!(black, APA102DataFrame::Led(0, 0, 0));
+        assert_eq!(black, APA102DataFrame(0, 0, 0));
 
         let white = APA102DataFrame::led_frame(0xffffff);
-        assert_eq!(white, APA102DataFrame::Led(255, 255, 255));
+        assert_eq!(white, APA102DataFrame(255, 255, 255));
     }
 
     #[test]
     fn it_builds_color_frames() {
         let red = APA102DataFrame::led_frame(0xff0000);
-        assert_eq!(red, APA102DataFrame::Led(255, 0, 0));
+        assert_eq!(red, APA102DataFrame(255, 0, 0));
 
         let green = APA102DataFrame::led_frame(0x00ff00);
-        assert_eq!(green, APA102DataFrame::Led(0, 255, 0));
+        assert_eq!(green, APA102DataFrame(0, 255, 0));
 
         let blue = APA102DataFrame::led_frame(0x0000ff);
-        assert_eq!(blue, APA102DataFrame::Led(0, 0, 255));
+        assert_eq!(blue, APA102DataFrame(0, 0, 255));
 
         let color = APA102DataFrame::led_frame(0x4b8040);
-        assert_eq!(color, APA102DataFrame::Led(75, 128, 64));
+        assert_eq!(color, APA102DataFrame(75, 128, 64));
     }
 
     #[test]
@@ -109,7 +118,7 @@ mod tests {
     #[test]
     fn it_makes_frames_for_a_single_led_strip() {
         let led_strip = LEDStrip::new_with_data([0x4b8040]);
-        assert_eq!(led_strip.data, [APA102DataFrame::Led(75, 128, 64)]);
+        assert_eq!(led_strip.data, [APA102DataFrame(75, 128, 64)]);
         assert_eq!(
             led_strip.get_spi_data(),
             &[
@@ -126,10 +135,10 @@ mod tests {
         assert_eq!(
             led_strip.data,
             [
-                APA102DataFrame::Led(255, 0, 0),
-                APA102DataFrame::Led(0, 255, 0),
-                APA102DataFrame::Led(0, 0, 255),
-                APA102DataFrame::Led(75, 128, 64),
+                APA102DataFrame(255, 0, 0),
+                APA102DataFrame(0, 255, 0),
+                APA102DataFrame(0, 0, 255),
+                APA102DataFrame(75, 128, 64),
             ]
         );
         assert_eq!(
@@ -147,15 +156,22 @@ mod tests {
     }
 
     #[test]
+    fn it_gets_rgb_values_of_individual_leds() {
+        let led_strip = LEDStrip::new_with_data([0xff0000, 0x00ff00, 0x0000ff, 0x4b8040]);
+        assert_eq!(led_strip.get_led(1), (0, 255, 0));
+        assert_eq!(led_strip.get_led(3), (75, 128, 64));
+    }
+
+    #[test]
     fn it_sets_an_led() {
         let mut led_strip = LEDStrip::new_with_data([0xff0000, 0x00ff00, 0x0000ff, 0x4b8040]);
         assert_eq!(
             led_strip.data,
             [
-                APA102DataFrame::Led(255, 0, 0),
-                APA102DataFrame::Led(0, 255, 0),
-                APA102DataFrame::Led(0, 0, 255),
-                APA102DataFrame::Led(75, 128, 64),
+                APA102DataFrame(255, 0, 0),
+                APA102DataFrame(0, 255, 0),
+                APA102DataFrame(0, 0, 255),
+                APA102DataFrame(75, 128, 64),
             ]
         );
         assert_eq!(
@@ -176,10 +192,10 @@ mod tests {
         assert_eq!(
             led_strip.data,
             [
-                APA102DataFrame::Led(255, 0, 0),
-                APA102DataFrame::Led(0, 255, 0),
-                APA102DataFrame::Led(243, 41, 178),
-                APA102DataFrame::Led(75, 128, 64),
+                APA102DataFrame(255, 0, 0),
+                APA102DataFrame(0, 255, 0),
+                APA102DataFrame(243, 41, 178),
+                APA102DataFrame(75, 128, 64),
             ]
         );
         assert_eq!(
